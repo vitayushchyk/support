de8c6b14f2951c154c73681f521fe087d4d1dda4
vitayushchyk <vitay2715@gmail.com>
2024-04-02 10:23:34 +0300

    Removed all migrations, added .env file

--- currency_exchange/currency_test/urls.py
+++ currency_exchange/currency_test/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from currency_test_app import views
 from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
-
-from currency_test_app import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
